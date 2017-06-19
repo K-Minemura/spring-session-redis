a1b2882f6ca78449e0842f2bcdc7d9e5beb9ea34
kmine <k.minemura27@gmail.com>
2017-06-19 16:10:06 +0900

    add no operation

--- src/main/java/jp/gr/java_conf/kmine27/spring/session/redis/config/SessionConfig.java
+++ src/main/java/jp/gr/java_conf/kmine27/spring/session/redis/config/SessionConfig.java
@@ -1,8 +1,17 @@
 package jp.gr.java_conf.kmine27.spring.session.redis.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
+@Configuration
 public class SessionConfig {
+
+    @Bean
+    public ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
 
 }

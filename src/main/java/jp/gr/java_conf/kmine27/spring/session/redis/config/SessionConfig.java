303a2f99f8300c7eb59091a1f2d1880fbf1140d5
kmine <k.minemura27@gmail.com>
2017-06-19 20:22:18 +0900

    Add Session Timeout

--- src/main/java/jp/gr/java_conf/kmine27/spring/session/redis/config/SessionConfig.java
+++ src/main/java/jp/gr/java_conf/kmine27/spring/session/redis/config/SessionConfig.java
@@ -1,7 +1,12 @@
 package jp.gr.java_conf.kmine27.spring.session.redis.config;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
@@ -14,4 +19,14 @@ public class SessionConfig {
         return ConfigureRedisAction.NO_OP;
     }
 
+    @Value("${server.session.timeout}")
+    private Integer maxInactiveIntervalInMinutes;
+
+    @Autowired
+    private RedisOperationsSessionRepository sessionRepository;
+
+    @PostConstruct
+    private void afterPropertiesSet() {
+        sessionRepository.setDefaultMaxInactiveInterval(maxInactiveIntervalInMinutes * 60);
+    }
 }
